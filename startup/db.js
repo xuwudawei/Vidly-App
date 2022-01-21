0455794075d3f5fdbbe5537ba879820dd0d0ca43
xuwudawei <xuwudawei@outlook.com>
2022-01-21 04:39:12 +0000

    redeployment

--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,5 @@ const config = require("config");
 
 module.exports = function () {
     mongoose.connect(config.get('db'),{ useNewUrlParser: true, uri_decode_auth: true })
-        .then(() => console.log(`Connected to ${db} successfully ...`));
+        .then(() => console.log(`Connected to ${db} successfully ....`));
 }
